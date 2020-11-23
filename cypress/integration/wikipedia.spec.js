e131e257936e4a7ab05a87e174a278da81ae1208
Katarzyna Kmiotek <katarzyna.kmiotek@2itesting.com>
2020-11-23 10:13:38 +0000

    cypress-axe

--- cypress/integration/wikipedia.spec.js
+++ cypress/integration/wikipedia.spec.js
@@ -3,8 +3,9 @@
 describe('Testing Wikipedia', () => {
     beforeEach(()=>{
         cy.visit('https://www.wikipedia.org');
+        cy.injectAxe();
     })
-    it('Can use search feature', () => {
+    it('Can use search feature on a homepage', () => {
       cy.get("input[type='search']").type('Nick Cave');
       cy.get("button[type='submit']").click();
       cy.contains('Where the Wild Roses Grow');
@@ -12,7 +13,7 @@ describe('Testing Wikipedia', () => {
       cy.contains("Song of the Year")
     });
 
-    it('Can switch languages', () => {
+    it('Can switch languages from homepage', () => {
         cy.contains('Read Wikipedia in your language');
         cy.get('#js-lang-list-button').click();
         cy.contains('Polski').click();
@@ -20,6 +21,16 @@ describe('Testing Wikipedia', () => {
           'equal',
           'https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna',
         );
-        
       });
+      it('can search in selected language from homepage', ()=>{
+        cy.get("input[type=search]").type("tiramisu");
+        cy.get("select[id=searchLanguage]").select("Italiano");
+        cy.get("button[type=submit]").click()
+        cy.contains("img")
+      })
+
+      it("checking accessibility of homepage: fail", ()=>{
+        cy.checkAccessibility();
+      })
+      
   });
